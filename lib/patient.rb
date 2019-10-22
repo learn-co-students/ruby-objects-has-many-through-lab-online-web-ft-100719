73752b5b8bf6653caa7e54e019813b94b76e6db8
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-22 20:01:57 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,15 @@
 class Patient
 
   attr_reader :name, :appointment, :doctor
+  all = []
 
   def initialize(name)
     @name = name
+    @@all = self
+  end
+
+  def self.all
+    @@all
   end
 
   def new_appointment(doctor, date)
