a8fc47cf0ef9e21796ea4d710e1e50d4b94aed90
Geoff Elliott <tinywv@gmail.com>
2019-10-24 20:33:42 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient
+
+  attr_accessor :name, :appointment, :doctor
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
